4aef81492b8b8ffeb5db1f74addd2838bc28a3dd
amdymstfa <amdymoustapha011@gmail.com>
2025-08-06 15:34:49 +0100

    Add IRedacteur interface for document and section management

---
+++ app/Interfaces/Redacteur.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Interfaces;
+
+interface IRedacteur
+{
+    // document
+    public function createDocument(string $titre, string $template) : IDocument;
+    public function updateDocument(IDocument $document) : bool;
+    public function deleteDocument(int $documentId) : bool;
+
+    //section
+    public function addSection(int $documentId, ISection $section) : bool;
+    public function updtateSection(ISection $section) : bool;
+    public function deleteSection(int $sectionId): bool;
+
+    // actions sur le document 
+    public function subitForValidation(int $documentId) : bool ;
+    public function canEdit(int $documentId) : bool ;
+}
